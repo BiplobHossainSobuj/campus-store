1e8999940e51fe2e0f01997cd599f634053ad037
BiploHossainSobuj <bhs.cse@outlook.com>
2023-09-03 16:13:45 +0600

    build navbar and make some minor changes to other files

--- src/components/home/Header/Header.jsx
+++ src/components/home/Header/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faMagnifyingGlass, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return (
         <div>
-            
+            <div className="navbar-container">
+                <div>
+                    <h3>Campus Store</h3>
+                </div>
+                <div className='nav-items'>
+                    <a href="">Home</a>
+                    <a href="">Category</a>
+                    <a href="">Service</a>
+                    <a href="">Contact</a>
+                    <a href="">About Us</a>
+                </div>
+                <div>
+                    <a href=""><FontAwesomeIcon icon={faMagnifyingGlass} /></a>
+                    <a href=""><FontAwesomeIcon icon={faCoffee} /></a>
+                    <a href=""><FontAwesomeIcon icon={faUser} /></a>
+                </div>
+            </div>
         </div>
     );
 };
